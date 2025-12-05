40a120bdceadb8567716c58233ae95cd59387457
beknazaarr <beknazaarr@gmail.com>
2025-12-06 02:51:23 +0600

    updating

--- frontend/src/pages/resume/ResumeEditPage.tsx
+++
@@ -1,8 +0,0 @@
-export default function ResumeEditPage() {
-  return (
-    <div className="space-y-6">
-      <h1 className="text-3xl font-bold">Редактировать резюме</h1>
-      <p className="text-gray-600">Страница редактирования резюме (в разработке)</p>
-    </div>
-  )
-}
